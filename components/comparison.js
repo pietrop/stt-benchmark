93d35d2e8cea5a435a8451fa5555bf4d2620fce2
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-04-23 00:26:10 -0400

    first commit

---
+++ components/comparison.js
@@ -0,0 +1,85 @@
+import React, {useState} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+const diffsListAsHtmlContentOnly = require('stt-align-node').diffsListAsHtmlContentOnly;
+const diffsCount = require('stt-align-node').diffsCount;
+
+export default function Comparison(props) { // Declare a new state variable, which we'll call "count"
+    const [comparisonData, setComparisonData] = useState(0);
+    const [htmlDiffContent, setHtmlDiffContent] = useState('');
+    const [diffCount, setDiffCount] = useState(null);
+    const [comparisonName, setComparisonName] = useState(`Click to set comparison name ${props.n}`);
+
+    
+
+    const handleAddComparisonData = (e) => {
+        const reader = new FileReader();
+        reader.onload = ()=>{
+          const text = reader.result;
+          // const node = document.getElementById('output');
+          // node.innerText = text;
+          const tmpComparisonData = JSON.parse(text);
+          console.log('tmpComparisonData',tmpComparisonData)
+          setComparisonData(tmpComparisonData);
+          console.log(reader.result.substring(0, 200));
+          console.log('tmpComparisonData',tmpComparisonData);
+          const tmpHtmlDiffContent = diffsListAsHtmlContentOnly(tmpComparisonData, props.baseText)
+          setHtmlDiffContent(tmpHtmlDiffContent);
+          const resultCount = diffsCount( tmpComparisonData, props.baseText);
+          setDiffCount(resultCount)
+        };
+        reader.readAsText(e.target.files[0]);
+
+     
+    }
+
+    const handleRemove = ()=>{
+        console.log('handleRemove',props.n)
+        props.handleRemoveComparison(props.n)
+    }
+
+    const handleSetComparisonName = ()=>{
+        const resp = prompt('comparison name')
+        if(resp){
+            setComparisonName(resp)
+        }
+    }
+    return (
+        <div>
+            <h2 onClick={handleSetComparisonName}>{comparisonName} {props.n}</h2>
+            <Button onClick={handleRemove}>Remove</Button>
+            {/* <p>You clicked {count} times</p> */}
+            <Form.File>
+                <Form.File.Label>Base text file input<code>.txt</code>
+                </Form.File.Label>
+                <Form.File.Input onChange={handleAddComparisonData}/>
+            </Form.File>
+            <section>
+                {diffCount && <>
+                <ul>
+                    <li>
+                        <span className="total">Total (base text):</span> {diffCount.baseTextTotalWordCount}
+                    </li>
+                    <li>
+                        <span className="equal">equal:</span> {diffCount.equal}
+                    </li>
+                    <li>
+                        <span className="insert">insert:</span> {diffCount.insert}
+                    </li>
+                    <li>
+                        <span class="replaceSTT ">replace:</span> {diffCount.replace}
+                    </li>
+                    <li>
+                        <span className="delete">delete:</span> {diffCount.delete}
+                    </li>
+                    <li>
+                        <strong title={'Word Error Rate, a measure for benchmarking stt transcript against accurate base text, lower WER indicates higher accuracy'}>WER:</strong>{(diffCount.replace + diffCount.delete + diffCount.insert)/diffCount.baseTextTotalWordCount}
+                    </li>
+                </ul>
+                </>}
+            </section>
+            <div dangerouslySetInnerHTML={{__html: htmlDiffContent}} />
+            {/* {htmlDiffContent} */}
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,204 +1,182 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import React, {useState, useEffect, useRef} from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Comparison from '../components/comparison.js';
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const [countList, setCountList] = useState([0]);
+    const [baseText, setBaseText] = useState('');
+    const [baseMedia, setBaseMedia] = useState('');
+    const videoElRef = useRef(null);
+    useEffect(() => {
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        videoElRef && videoElRef.current && videoElRef.current.addEventListener("timeupdate", function(){
+            console.log('time updated', this.currentTime)
+            let currentTime = this.currentTime;
+            let wordsEl = document.querySelectorAll('.word');
+            wordsEl.forEach((word)=>{
+                if(word.dataset.start >= currentTime){
+                    word.classList.add("unplayedWord");
+                }
+                else{
+                    word.classList.remove("unplayedWord");
+                }
+            })
+        });
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+    },[   ])
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    const increaseCount = () => {
+        console.log('increaseCount')
+        countList.push(countList.length);
+        const tmpCount = JSON.parse(JSON.stringify(countList));
+        setCountList(tmpCount);
+    }
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    const decreaseCount = () => {
+        countList.pop();
+        const tmpCount = JSON.parse(JSON.stringify(countList));
+        setCountList(tmpCount);
+    }
 
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+    const handleBaseTextFileInput =(e)=>{
+      console.log(e.target.files[0].name,e.target.files[0]);
+      const reader = new FileReader();
+        reader.onload = ()=>{
+          const text = reader.result;
+          // const node = document.getElementById('output');
+          // node.innerText = text;
+          setBaseText(text);
+          console.log(reader.result.substring(0, 200));
+        };
+        reader.readAsText(e.target.files[0]);
+    }
 
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
+    const handleSetBaseMedia=(e)=>{
+      const url = window.URL.createObjectURL(e.target.files[0]);
+      setBaseMedia(url)
+    }
+
+    const handleRemoveComparison = (n)=>{
+      console.log('handleRemoveComparison parent',n)
+      const tmpCount = JSON.parse(JSON.stringify(countList));
+      // const index = tmpCount.indexOf(n);
+      // if (index > -1) {
+      //   tmpCount.splice(index, 1);
+      // }
+      delete tmpCount[n];
+      setCountList(tmpCount);
+    }
+    const handleWordClick = (e)=>{
+        if(e.target.classList.contains('word')){
+           const startTime = e.target.dataset.start;
+           if(videoElRef&&videoElRef.current){
+              try {
+                videoElRef.current.currentTime = startTime;//parseFloat(parseFloat(startTime).toFixed(2));
+                videoElRef.current.play();
+              }catch(e){
+                  console.error('couldn\'t play ',e)
+              }
+           }
+        }
+    }
+    return (
+        <div>
+            <Head>
+                <title>STT Benchmark</title>
+            </Head>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <h1>STT Benchmarking</h1>
+                        {/* <p>You clicked { countList.length }  times</p> */}
+                      
+                        {/* <Form>
+                          <Form.File 
+                            id="custom-file"
+                            label="Select base text  file"
+                            custom
+                          />
+                        </Form> */}
+                        <Form.File>
+                          <Form.File.Label>Base text file input<code>.txt</code></Form.File.Label>
+                          <Form.File.Input onChange={handleBaseTextFileInput}/>
+                        </Form.File>
+
+                        <Form.File>
+                          <Form.File.Label>Video or audio file <code>mp4</code>,<code>ogg</code>,<code>webm</code>,<code>wav</code>,<code>mp3</code></Form.File.Label>
+                          <Form.File.Input onChange={handleSetBaseMedia}/>
+                        </Form.File>
+                      
+                    </Col>
+                    <Col>
+                    { baseText && countList.length >1 ?    <div> 
+                      <h3>Leggend</h3>
+                        Equal: Some equal text
+                        <br/>
+                        Inserted (by STT): <span class='insert'>an</span> <span class='insert'>inserted</span> <span class='insert'>word</span>
+                        <br/>
+                        Deleted (not recognised by STT):<span class='delete'>a</span>  <span class='delete'>deleted</span> <span class='delete'>word</span>
+                        <br/>
+                        Replaced:<span class='replaceBaseTextLine line'>
+                        <span class='replaceBaseText '>Some</span>
+                        <span class='replaceBaseText '>base</span>
+                        <span class='replaceBaseText '>text</span>
+                        <span class='replaceBaseText '>line</span>
+                        </span>
+                        <span class='replaceSttLine line'>
+                        <span class="replaceStt ">replaced</span>
+                        <span class="replaceStt ">by</span>
+                        <span class="replaceStt ">stt</span>
+                        <span class="replaceStt ">hypothesis</span>
+                        </span>
+                    </div> : null}
+                    </Col>
+                    <Col>
+                     {/* {baseMedia&&   */}
+                      <video  ref={videoElRef}  className='videoPreview' src={baseMedia} style={ {  width: '100%',  height: '200px' }  }  controls></video>
+                      {/* } */}
+                    </Col>
+                </Row>
+
+            </Container>
+            <Container fluid>
+                <Row>
+                    <Col>
+                   { baseText &&  <Button block
+                            onClick={increaseCount}>
+                            Add a new comparison 
+                        </Button>}
+                    </Col>
+                    {/* <Col>
+                        <Button block onClick={decreaseCount}>
+                            Remove One
+                        </Button>
+                    </Col> */} </Row>
+                    <Row>
+                  
+                    </Row>
+                <Row onClick={handleWordClick}> 
+                {/* {baseText && <Col><h2>Base Text</h2><p>{baseText}</p></Col>} */}
+                    {baseText &&
+                    countList.map((n, index) => {
+                      if(n){
+                        return <Col key={index+n}><Comparison handleRemoveComparison={handleRemoveComparison} n={n} baseText={baseText}/></Col>
+                      }
+                })
+                } </Row>
+            </Container>
         </div>
-      </main>
+    )
+}
 
-      <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-        </a>
-      </footer>
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+export async function getStaticProps(context) {
+    return {
+        props: {}, // will be passed to the page component as props
+    }
 }
